9ed9d2c2115d947e07caf06f56d93b93ebb49af1
Evan Morrison <emorrison@gmail.com>
2020-08-02 17:28:23 -0700

    fix snapshot plot app

--- R/app-snapshot.R
+++ R/app-snapshot.R
@@ -1,34 +1,61 @@
+#' Shiny app to plot snapshot of NDBC marine weather data
+#'
+#' @return
+#' @import shiny
+#' @importFrom magrittr '%<>%'
+#' @export
+#'
+#' @examples
 snapshotApp <- function() {
   server <- function(input, output) {
 
-    data <- reactive({
-      if (input$submit == 0) { return(NULL) }
+    rv <- reactiveValues()
 
+    observeEvent(input$submit, {
+      shinyjs::show("tz_adj")
+      shinyjs::show("hoursUI")
+      shinyjs::hide("daterange")
       date1 <- input$daterange[1]
       date2 <- input$daterange[2]
-
       cols <- c("date", input$features)
 
-      withProgress(message = 'Downloading from NOAA...', {
-        d <- get_buoy(input$buoyid, scope = "histY", date = year(date1))
-      })
+      withProgress(
+        message = 'Downloading from NOAA...',
+        rv$d <- ndbc_window(input$buoyid, date1, date2)
+      )
+    })
 
-      if (year(date1) != year(date2)) {
-        y2 <- get_buoy(input$buoyid, scope = "histY", date = year(date2))
-        d <- rbind(d, y2)
-      }
+    output$hoursUI <- renderUI({
+      req(rv$d)
+      sliderInput(
+        "hours",
+        label = "Head",
+        min = min(rv$d$date),
+        max = max(rv$d$date),
+        value = c(min(rv$d$date), max(rv$d$date))
+      )
+    })
 
-      d <- subset(d, as.Date(date) >= date1 & as.Date(date) < date2)
-      d[c("date", input$features)]
+    observeEvent(input$hours,
+      rv$d %<>% filter(
+        date >= input$hours[1],
+        date <= input$hours[2]
+    ))
+
+    observeEvent(input$tz_adj, {
+      req(rv$d)
+      rv$d %<>% mutate(
+        date = date + lubridate::hours(input$tz_adj)
+      )
     })
 
     output$dt <- renderDataTable({
-      req(data())
-      data()
+      req(rv$d)
+      rv$d
     }, options = list(pageLength = 10, searching = F, rownames = F))
 
     output$plotTabs <- renderUI({
-      req(data())
+      req(rv$d)
       tabsetPanel(
         tabPanel("WTMP", plotOutput("wtmp")),
         tabPanel("ATMP", plotOutput("atmp")),
@@ -37,20 +64,22 @@ snapshotApp <- function() {
       )
     })
 
-    output$wtmp <- renderPlot(qplot(date, WTMP, data = data(), geom = "line"))
-    output$atmp <- renderPlot(qplot(date, ATMP, data = data(), geom = "line"))
-    output$wspd <- renderPlot(qplot(date, WSPD, data = data(), geom = "line"))
-    output$wvht <- renderPlot(qplot(date, WVHT, data = data(), geom = "line"))
+    output$wtmp <- renderPlot(qplot(date, WTMP, data = rv$d, geom = "line"))
+    output$atmp <- renderPlot(qplot(date, ATMP, data = rv$d, geom = "line"))
+    output$wspd <- renderPlot(qplot(date, WSPD, data = rv$d, geom = "line"))
+    output$wvht <- renderPlot(qplot(date, WVHT, data = rv$d, geom = "line"))
   }
 
-  ui <- fluidPage(sidebarLayout(
+  ui <- fluidPage(shinyjs::useShinyjs(), sidebarLayout(
     sidebarPanel(
       textInput("buoyid", label = "Enter Buoy ID"),
-      checkboxGroupInput("features", label = "Choose Features", choices =
-                           c("WTMP", "ATMP", "WSPD", "WVHT")
-      ),
       dateRangeInput("daterange", label = "Choose Date Range"),
-      actionButton("submit", "Get Data")
+      actionButton("submit", "Get Data"),
+      shinyjs::hidden(sliderInput(
+        "tz_adj", label = "Adjust Timezone",
+         min = -12, max = 12, value = 0, step = 1)
+      ),
+      shinyjs::hidden(uiOutput("hoursUI"))
     ),
     mainPanel(
       uiOutput("plotTabs"),
